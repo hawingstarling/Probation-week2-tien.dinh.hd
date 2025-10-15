b62ac1aac3e229a6d2522b5bbd475e566c598e22
Ngo Nham HD <ngo.nham@hdwebsoft.dev>
2025-10-15 13:45:13 +0700

    feat(views): crud posts stored in PostgreSQL, using django admin to manage posts

--- blogs/views.py
+++ blogs/views.py
@@ -1,3 +1,31 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404, render, redirect
+from .models import Post
 
 # Create your views here.
+def index(request):
+  posts = Post.objects.all().order_by('-created_at')
+  return render(request, 'index.html', {'posts': posts})
+
+def create(request):
+  if request.method == 'POST':
+    title = request.POST.get('title')
+    body = request.POST.get('body')
+    Post.objects.create(title=title, body=body)
+    return redirect('/')
+  return render(request, 'create.html')
+
+def edit(request, post_id):
+    post = get_object_or_404(Post, id=post_id)
+    
+    if request.method == 'POST':
+        post.title = request.POST.get('title')
+        post.body = request.POST.get('body')
+        post.save()
+        return redirect('/')
+    
+    return render(request, 'edit.html', {'post': post})
+
+def delete(request, post_id):
+    post = get_object_or_404(Post, id=post_id)
+    post.delete()
+    return redirect('/')
